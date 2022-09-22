8214a689e6baeac60011a7d982c6e8d372456e18
Ersan Karimi <73420137+ersankarimi@users.noreply.github.com>
2022-09-22 16:05:07 +0800

    Update script.js
    Make findUniqValue function to get uniq value of array

---
+++ project-javascript-personal-1/script.js
@@ -0,0 +1,18 @@
+// Find uniqe value of array (string or number)
+const findUniqValue = (array, type) => {
+	if (type === "number") {
+		return array.filter(
+			(num) => array.indexOf(num) === array.lastIndexOf(num)
+		)[0];
+	}
+	const strOfSetArr = array.map((str) =>
+		[...new Set([...str.toLowerCase()])].sort().join("")
+	);
+
+	const uniqeValueIndex = strOfSetArr.findIndex(
+		(value, _, strArr) =>
+			strArr.indexOf(value) === strArr.lastIndexOf(value)
+	);
+
+	return array[uniqeValueIndex];
+};
