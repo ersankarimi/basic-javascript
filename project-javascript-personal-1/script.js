165b9caf61d6c46d16e9a1281683a9b59d6f5f95
Ersan Karimi <ersankarimii311@gmail.com>
2022-09-24 20:55:17 +0800

    Update script.js
    - Renaming variables strArrOfCharsValue to strArrOfCharsValues
    - Return "Enter parameters type with a value between number or string" when type is not equal "number" or "string"
    - Return "Please enter the number of array values of at least 3" when values of array less of 3

--- project-javascript-personal-1/script.js
+++ project-javascript-personal-1/script.js
@@ -1,17 +1,23 @@
 // Find uniqe value of array (string or number)
 const findUniqValue = (array, type) => {
-	if (type !== "number" && type !== "string") return;
+	if (type !== "number" && type !== "string") {
+		return "Enter parameters type with a value between number or string";
+	}
+
+	if (array.length < 3)
+		return "Please enter the number of array values of at least 3";
 
 	if (type === "number") {
 		return array.filter(
 			(num) => array.indexOf(num) === array.lastIndexOf(num)
 		)[0];
 	}
-	const strArrOfCharsValue = array.map((str) =>
+
+	const strArrOfCharsValues = array.map((str) =>
 		[...new Set([...str.toLowerCase()])].sort().join("")
 	);
 
-	const uniqeValueIndex = strArrOfCharsValue.findIndex(
+	const uniqeValueIndex = strArrOfCharsValues.findIndex(
 		(value, _, strArr) =>
 			strArr.indexOf(value) === strArr.lastIndexOf(value)
 	);
