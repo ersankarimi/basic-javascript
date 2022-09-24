c4c73bc2919ed33c4cecbe8ba3f7722b6a04c83c
Ersan Karimi <ersankarimii311@gmail.com>
2022-09-24 21:04:00 +0800

    Update script.js
    Change return to throw new Error

--- project-javascript-personal-1/script.js
+++ project-javascript-personal-1/script.js
@@ -1,11 +1,15 @@
 // Find uniqe value of array (string or number)
 const findUniqValue = (array, type) => {
 	if (type !== "number" && type !== "string") {
-		return "Enter parameters type with a value between number or string";
+		throw new Error(
+			"Enter parameters type with a value between number or string"
+		);
 	}
 
 	if (array.length < 3)
-		return "Please enter the number of array values of at least 3";
+		throw new Error(
+			"Please enter the number of array values of at least 3"
+		);
 
 	if (type === "number") {
 		return array.filter(
